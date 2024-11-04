0067394b43161f71648704679d2029e7d4af82d0
Fabio Santos <fabio.santos@ifrr.edu.br>
2024-11-04 11:05:15 -0400

    Fix tranlations

--- src/aratinga/settings.py
+++ src/aratinga/settings.py
@@ -12,58 +12,58 @@ class _DefaultSettings:
 
     CMS_FRONTEND_BTN_SIZE_DEFAULT = ""
     CMS_FRONTEND_BTN_SIZE_CHOICES = [
-        ("btn-sm", "Small"),
-        ("", "Default"),
-        ("btn-lg", "Large"),
+        ("btn-sm", _("Small")),
+        ("", _("Default")),
+        ("btn-lg", _("Large")),
     ]
 
     CMS_FRONTEND_BTN_STYLE_DEFAULT = "btn-primary"
     CMS_FRONTEND_BTN_STYLE_CHOICES = [
-        ("btn-primary", "Primary"),
-        ("btn-secondary", "Secondary"),
-        ("btn-success", "Success"),
-        ("btn-danger", "Danger"),
-        ("btn-warning", "Warning"),
-        ("btn-info", "Info"),
-        ("btn-link", "Link"),
-        ("btn-light", "Light"),
-        ("btn-dark", "Dark"),
-        ("btn-outline-primary", "Outline Primary"),
-        ("btn-outline-secondary", "Outline Secondary"),
-        ("btn-outline-success", "Outline Success"),
-        ("btn-outline-danger", "Outline Danger"),
-        ("btn-outline-warning", "Outline Warning"),
-        ("btn-outline-info", "Outline Info"),
-        ("btn-outline-light", "Outline Light"),
-        ("btn-outline-dark", "Outline Dark"),
+        ("btn-primary", _("Primary")),
+        ("btn-secondary", _("Secondary")),
+        ("btn-success", _("Success")),
+        ("btn-danger", _("Danger")),
+        ("btn-warning", _("Warning")),
+        ("btn-info", _("Info")),
+        ("btn-link", _("Link")),
+        ("btn-light", _("Light")),
+        ("btn-dark", _("Dark")),
+        ("btn-outline-primary", _("Outline Primary")),
+        ("btn-outline-secondary", _("Outline Secondary")),
+        ("btn-outline-success", _("Outline Success")),
+        ("btn-outline-danger", _("Outline Danger")),
+        ("btn-outline-warning", _("Outline Warning")),
+        ("btn-outline-info", _("Outline Info")),
+        ("btn-outline-light", _("Outline Light")),
+        ("btn-outline-dark", _("Outline Dark")),
     ]
 
     
     CMS_FRONTEND_COL_SIZE_DEFAULT = ""
     CMS_FRONTEND_COL_SIZE_CHOICES = [
-        ("", "Automatically size"),
-        ("12", "Full row"),
-        ("6", "Half - 1/2 column"),
-        ("4", "Thirds - 1/3 column"),
-        ("8", "Thirds - 2/3 column"),
-        ("3", "Quarters - 1/4 column"),
-        ("9", "Quarters - 3/4 column"),
-        ("2", "Sixths - 1/6 column"),
-        ("10", "Sixths - 5/6 column"),
-        ("1", "Twelfths - 1/12 column"),
-        ("5", "Twelfths - 5/12 column"),
-        ("7", "Twelfths - 7/12 column"),
-        ("11", "Twelfths - 11/12 column"),
+        ("", _("Automatically size")),
+        ("12", _("Full row")),
+        ("6", _("Half - 1/2 column")),
+        ("4", _("Thirds - 1/3 column")),
+        ("8", _("Thirds - 2/3 column")),
+        ("3", _("Quarters - 1/4 column")),
+        ("9", _("Quarters - 3/4 column")),
+        ("2", _("Sixths - 1/6 column")),
+        ("10", _("Sixths - 5/6 column")),
+        ("1", _("Twelfths - 1/12 column")),
+        ("5", _("Twelfths - 5/12 column")),
+        ("7", _("Twelfths - 7/12 column")),
+        ("11", _("Twelfths - 11/12 column")),
     ]
 
 
     CMS_FRONTEND_COL_BREAK_DEFAULT = "md"
     CMS_FRONTEND_COL_BREAK_CHOICES = [
-        ("", "Always expanded"),
-        ("sm", "sm - Expand on small screens (phone, 576px) and larger"),
-        ("md", "md - Expand on medium screens (tablet, 768px) and larger"),
-        ("lg", "lg - Expand on large screens (laptop, 992px) and larger"),
-        ("xl", "xl - Expand on extra large screens (wide monitor, 1200px)"),
+        ("", _("Always expanded")),
+        ("sm", _("sm - Expand on small screens (phone, 576px) and larger")),
+        ("md", _("md - Expand on medium screens (tablet, 768px) and larger")),
+        ("lg", _("lg - Expand on large screens (laptop, 992px) and larger")),
+        ("xl", _("xl - Expand on extra large screens (wide monitor, 1200px)")),
     ]
 
     CMS_FRONTEND_TEMPLATES_PAGES = {
@@ -112,46 +112,46 @@ class _DefaultSettings:
         "cardblock": [
             (
                 "aratinga/blocks/card_block.html",
-                "Card",
+                _("Card"),
             ),
             (
                 "aratinga/blocks/card_head.html",
-                "Card with header",
+                _("Card with header"),
             ),
             (
                 "aratinga/blocks/card_foot.html",
-                "Card with footer",
+                _("Card with footer"),
             ),
             (
                 "aratinga/blocks/card_head_foot.html",
-                "Card with header and footer",
+                _("Card with header and footer"),
             ),
             (
                 "aratinga/blocks/card_blurb.html",
-                "Blurb - rounded image and no border",
+                _("Blurb - rounded image and no border"),
             ),
             (
                 "aratinga/blocks/card_img.html",
-                "Cover image - use image as background",
+                _("Cover image - use image as background"),
             ),
         ],
         "cardgridblock": [
             (
                 "aratinga/blocks/cardgrid_group.html",
-                "Card group - attached cards of equal size",
+                _("Card group - attached cards of equal size"),
             ),
             (
                 "aratinga/blocks/cardgrid_deck.html",
-                "Card deck - separate cards of equal size",
+                _("Card deck - separate cards of equal size"),
             ),
             (
                 "aratinga/blocks/cardgrid_columns.html",
-                "Card masonry - fluid brick pattern",
+                _("Card masonry - fluid brick pattern"),
             ),
         ],
         # templates that are available for all block types
         "*": [
-            ("", "Default"),
+            ("", _("Default")),
         ],
     }
 
@@ -160,23 +160,23 @@ class _DefaultSettings:
         "*": [
             (
                 "",
-                "Default",
+                _("Default"),
             ),
             (
                 "aratinga/pages/web_page.html",
-                "Web page showing title and cover image",
+                _("Web page showing title and cover image"),
             ),
             (
                 "aratinga/pages/web_page_notitle.html",
-                "Web page without title and cover image",
+                _("Web page without title and cover image"),
             ),
             (
                 "aratinga/pages/home_page.html",
-                "Home page without title and cover image",
+                _("Home page without title and cover image"),
             ),
             (
                 "aratinga/pages/base.html",
-                "Blank page - no navbar or footer",
+                _("Blank page - no navbar or footer"),
             ),
         ],
     }
