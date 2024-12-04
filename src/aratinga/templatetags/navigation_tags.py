5163d21d4446f270b939722aef6f561b65cd922e
Fábio Rodrigues <f4bio.sa@gmail.com>
2024-12-04 10:06:00 -0400

    Title  suffix and navigation_tags

--- src/aratinga/templatetags/navigation_tags.py
+++ src/aratinga/templatetags/navigation_tags.py
@@ -13,6 +13,13 @@ def get_site_root(context):
     return Site.find_for_request(context["request"]).root_page
 
 
+def has_menu_children(page):
+    # This is used by the top_menu property
+    # get_children is a Treebeard API thing
+    # https://tabo.pe/projects/django-treebeard/docs/4.0.1/api.html
+    return page.get_children().live().in_menu().exists()
+
+
 def has_children(page):
     # Generically allow index pages to list their children
     return page.get_children().live().exists()
@@ -43,6 +50,46 @@ def top_menu(context, parent, calling_page=None):
         "request": context["request"],
     }
 
+@register.inclusion_tag("tags/sitemap.html", takes_context=True)
+def sitemap(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (
+            calling_page.url_path.startswith(menuitem.url_path)
+            if calling_page
+            else False
+        )
+    return {
+        "calling_page": calling_page,
+        "menuitems": menuitems,
+        "request": context["request"],
+    }
+
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag("tags/top_menu_children.html", takes_context=True)
+def top_menu_children(context, parent, calling_page=None):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    for menuitem in menuitems_children:
+        menuitem.has_dropdown = has_menu_children(menuitem)
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (
+            calling_page.url_path.startswith(menuitem.url_path)
+            if calling_page
+            else False
+        )
+        menuitem.children = menuitem.get_children().live().in_menu()
+    return {
+        "parent": parent,
+        "menuitems_children": menuitems_children,
+        # required by the pageurl tag that we want to use within this template
+        "request": context["request"],
+    }
+
 
 @register.inclusion_tag("aratinga/tags/breadcrumbs.html", takes_context=True)
 def breadcrumbs(context):
@@ -55,4 +102,21 @@ def breadcrumbs(context):
     return {
         "ancestors": ancestors,
         "request": context["request"],
+    }
+
+
+@register.inclusion_tag("includes/footer-text.html", takes_context=True)
+def get_footer_text(context):
+    # Get the footer text from the context if exists,
+    # so that it's possible to pass a custom instance e.g. for previews
+    # or page types that need a custom footer
+    footer_text = context.get("footer_text", "")
+
+    # If the context doesn't have footer_text defined, get one that's live
+    if not footer_text:
+        instance = FooterText.objects.filter(live=True).first()
+        footer_text = instance.body if instance else ""
+
+    return {
+        "footer_text": footer_text,
     }
